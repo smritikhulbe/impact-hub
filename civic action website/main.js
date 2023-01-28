3686b05903f4191cd70f72a6630f327fa52f6ea9
smritikhulbe <smritikhulbe@gmail.com>
2023-01-28 17:25:17 -0600

    First Draft
    First draft of Impact Hub.

---
+++ civic action website/main.js
@@ -0,0 +1,9 @@
+const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+});
+
+const marker = new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644 },
+    map: map
+});
