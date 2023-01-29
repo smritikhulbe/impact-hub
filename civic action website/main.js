dcf549b456671b9c6658c76982ccb9b7dcb536ff
smritikhulbe <smritikhulbe@gmail.com>
2023-01-28 18:33:46 -0600

    New changes
    New changes to the code.

--- civic action website/main.js
+++ civic action website/main.js
@@ -1,9 +1,33 @@
-const map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-});
+function createMarker(map, position, content) {
+    const marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+    const infowindow = new google.maps.InfoWindow({
+        content: content
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+    return marker;
+}
 
-const marker = new google.maps.Marker({
-    position: { lat: -34.397, lng: 150.644 },
-    map: map
-});
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+    const contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Marker Description</h1>' +
+        '<div id="bodyContent">' +
+        '<p>Here is some information about the marker.</p>' +
+        '</div>' +
+        '</div>';
+    createMarker(map, { lat: -34.397, lng: 150.644 }, contentString)
+    google.maps.event.addListener(map, 'click', function() {
+        infowindow.close();
+    });
+}
+initMap();
